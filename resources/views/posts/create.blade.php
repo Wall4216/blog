197ebf4b87ed287d40a76762002f7a625c755693
Wall <ilnazamirhanov61@gmail.com>
2023-06-04 23:44:19 +0300

    add styles

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,33 +2,35 @@
 <html>
 <head>
     <title>Blog - Create Post</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-<h1>Create Post</h1>
+<div class="container">
+    <h1 class="mt-4">Create Post</h1>
 
-@if ($errors->any())
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+    @if ($errors->any())
+        <div class="alert alert-danger mt-4">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-<form method="POST" action="/posts">
-    @csrf
-    <div>
-        <label for="title">Title:</label>
-        <input type="text" id="title" name="title" required>
-    </div>
-    <div>
-        <label for="content">Content:</label>
-        <textarea id="content" name="content" required></textarea>
-    </div>
-    <div>
-        <button type="submit">Create</button>
-    </div>
-</form>
-</body>
-</html>
+    <form method="POST" action="/posts" class="mt-4">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Title:</label>
+            <input type="text" id="title" name="title" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content:</label>
+            <textarea id="content" name="content" class="form-control" required></textarea>
+        </div>
+        <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Create</button>
+        </div>
+    </form>
+</div>
+
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,9 +2,14 @@
 <html>
 <head>
     <title>Blog - Posts</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
         .post {
             margin-bottom: 10px;
+            padding: 10px;
+            background-color: #f8f9fa;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
         .delete-form {
             display: inline;
@@ -13,21 +18,27 @@
     </style>
 </head>
 <body>
-<h1>Blog - Posts</h1>
+<div class="container">
+    <h1 class="mt-4">Blog - Posts</h1>
 
-<a href="/posts/create">Create New Post</a>
+    <a href="/posts/create" class="btn btn-primary my-3">Create New Post</a>
 
-<ul>
-    @foreach ($posts as $post)
-        <li class="post">
-            {{ $post->title }}
-            <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="d-inline">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Удалить</button>
-            </form>
-        </li>
-    @endforeach
-</ul>
+    <ul class="list-group">
+        @foreach ($posts as $post)
+            <li class="list-group-item post">
+                <div class="d-flex justify-content-between">
+                    <h5>{{ $post->title }}</h5>
+                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="delete-form">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.5.0/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
